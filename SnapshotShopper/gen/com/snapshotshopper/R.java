f143d00a8b823df835e5ca7a5f2dd1824ecf7cd1
Aya Yakura <aya2@pdx.edu>
2012-07-30 17:24:43 -0700

    Added function to add items to list, and clear list

--- SnapshotShopper/gen/com/snapshotshopper/R.java
+++ SnapshotShopper/gen/com/snapshotshopper/R.java
@@ -8,9 +8,6 @@
 package com.snapshotshopper;
 
 public final class R {
-    public static final class array {
-        public static final int colors=0x7f060000;
-    }
     public static final class attr {
     }
     public static final class dimen {
@@ -23,9 +20,10 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int ShoppingList=0x7f090001;
-        public static final int edit_message=0x7f090000;
-        public static final int menu_settings=0x7f090002;
+        public static final int ClearBtn=0x7f080002;
+        public static final int ShoppingList=0x7f080001;
+        public static final int edit_message=0x7f080000;
+        public static final int menu_settings=0x7f080003;
     }
     public static final class layout {
         public static final int activity_display_message=0x7f030000;
@@ -33,21 +31,22 @@ public final class R {
         public static final int activity_snapshot_shopper=0x7f030002;
     }
     public static final class menu {
-        public static final int activity_display_message=0x7f080000;
-        public static final int activity_shopping_list=0x7f080001;
-        public static final int activity_snapshot_shopper=0x7f080002;
+        public static final int activity_display_message=0x7f070000;
+        public static final int activity_shopping_list=0x7f070001;
+        public static final int activity_snapshot_shopper=0x7f070002;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
+        public static final int button_clear=0x7f050007;
         public static final int button_send=0x7f050004;
         public static final int edit_message=0x7f050001;
         public static final int hello_world=0x7f050005;
         public static final int menu_settings=0x7f050002;
         public static final int title_activity_display_message=0x7f050006;
-        public static final int title_activity_shopping_list=0x7f050007;
+        public static final int title_activity_shopping_list=0x7f050008;
         public static final int title_activity_snapshot_shopper=0x7f050003;
     }
     public static final class style {
-        public static final int AppTheme=0x7f070000;
+        public static final int AppTheme=0x7f060000;
     }
 }
--- SnapshotShopper/src/com/snapshotshopper/SnapshotShopper.java
+++ SnapshotShopper/src/com/snapshotshopper/SnapshotShopper.java
@@ -1,5 +1,7 @@
 package com.snapshotshopper;
 
+import java.util.ArrayList;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
@@ -17,19 +19,25 @@ public class SnapshotShopper extends Activity {
 	//public constant for the next activity to query the extra data
 	public final static String EXTRA_MESSAGE = "com.snapshotshopper.MESSAGE";
 	
+	//ShoppingListItems contains the items for the list
+        ArrayList<String> ShoppingListItems = new ArrayList<String>();
+        //make a string adapter for notifying list got changed
+        ArrayAdapter<String> listAdapter;
+        int dummy = 1;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_snapshot_shopper);
         
-        String[] ShoppingListItems={"apple", "pear", "grape", "chicken", "milk"};
-        
         ListView ShoppingListView = (ListView)findViewById(R.id.ShoppingList);
         
-        ShoppingListView.setAdapter(new ArrayAdapter<String>(this,
-        		android.R.layout.simple_list_item_multiple_choice, ShoppingListItems));
+        listAdapter = new ArrayAdapter<String>(this,
+        		android.R.layout.simple_list_item_multiple_choice, ShoppingListItems);
         ShoppingListView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
         ShoppingListView.setItemsCanFocus(false);
+        ShoppingListView.setAdapter(listAdapter);
+        
         
 //        ShoppingListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 //            public void onItemClick(AdapterView<?> parent, View view,
@@ -48,13 +56,37 @@ public class SnapshotShopper extends Activity {
     
 
 //    called when the user clicks the Send button
-    public void sendMessage(View view){
-    	//Do something in response to button
-    	Intent intent = new Intent(this, DisplayMessageActivity.class);
-    	EditText editText = (EditText) findViewById(R.id.edit_message);
-    	String message = editText.getText().toString();
-    	intent.putExtra(EXTRA_MESSAGE, message);
-    	startActivity(intent);
+//Add the item to the list when the button is clicked    
+    public void addToList(View view){
+    	//Intent intent = new Intent(this, DisplayMessageActivity.class);
+    	//EditText editText = (EditText) findViewById(R.id.edit_message);
+    	//String message = editText.getText().toString();
+    	//intent.putExtra(EXTRA_MESSAGE, message);
+    	//startActivity(intent);
+
+    	//to use the textbox
+    	EditText enteredText = (EditText) findViewById(R.id.edit_message);
+    	//holds the entry by the user
+    	String entry = "";
+    	entry = enteredText.getText().toString(); //get the text entered
+    	//if the text box was not empty
+    	if (!entry.equals("") && !entry.equals("\n")){
+    		ShoppingListItems.add(entry);
+    		//notify that list got updated so it can refresh the list on screen
+    		listAdapter.notifyDataSetChanged();
+    	}
+    	
+
+    	//clear the text box so the user can enter another item
+    	//findViewById(R.id.edit_message)
+    }
+    
+    //clear the list when user clicks the Clear List button
+    public void clearList(View view){
+    	//clear list
+    	ShoppingListItems.clear();
+    	//notify that list got updated so it can refresh the list on screen
+    	listAdapter.notifyDataSetChanged();
     }
 }
 
