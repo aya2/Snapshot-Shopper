e899ad1ac656ad81b8bb548a1f25c87562e97d1f
Aya Yakura <aya2@pdx.edu>
2012-08-08 17:27:15 -0700

    Added license information to files

--- SnapshotShopper/src/com/snapshotshopper/SnapshotShopper.java
+++ SnapshotShopper/src/com/snapshotshopper/SnapshotShopper.java
@@ -1,3 +1,27 @@
+//***********************************************************************************
+// Copyright (C) 2012  Aya Yakura
+//
+ /* This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+//
+// Contact information:
+// email: aya2@pdx.edu
+
+//Portions of this page are modifications based on work created and shared by 
+//the Android Open Source Project and used according to terms described in the 
+//Creative Commons 2.5 Attribution License. 
+//***********************************************************************************
+
 package com.snapshotshopper;
 
 import java.util.ArrayList;
@@ -21,11 +45,16 @@ public class SnapshotShopper extends Activity {
 	//public constant for the next activity to query the extra data
 	public final static String EXTRA_MESSAGE = "com.snapshotshopper.MESSAGE";
 	
-	//ShoppingListItems contains the items for the list
+	//Intent intent = new Intent(this, DisplayMessageActivity.class);
+	//EditText editText = (EditText) findViewById(R.id.edit_message);
+	//String message = editText.getText().toString();
+	//intent.putExtra(EXTRA_MESSAGE, message);
+	//startActivity(intent);listItems contains the items for the list
+
         ArrayList<String> ShoppingListItems = new ArrayList<String>();
         //make a string adapter for notifying list got changed
         ArrayAdapter<String> listAdapter;
-        int dummy = 1;
+        //int dummy = 1;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -34,13 +63,18 @@ public class SnapshotShopper extends Activity {
         
         ListView ShoppingListView = (ListView)findViewById(R.id.ShoppingList);
         
+      //**************************************************************************************************
+      //Modified from following source:
+      //Source: http://stackoverflow.com/questions/4540754/add-dynamically-elements-to-a-listview-android
+      //Author: Shardul
+      //Author's profile page: http://stackoverflow.com/users/514553/shardul
         listAdapter = new ArrayAdapter<String>(this,
         		android.R.layout.simple_list_item_multiple_choice, ShoppingListItems);
         ShoppingListView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
         ShoppingListView.setItemsCanFocus(false);
         ShoppingListView.setAdapter(listAdapter);
         
-        
+      //**************************************************************************************************
 //        ShoppingListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 //            public void onItemClick(AdapterView<?> parent, View view,
 //                    int position, long id) {
@@ -49,22 +83,22 @@ public class SnapshotShopper extends Activity {
 //
 //        });
     }
-
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_snapshot_shopper, menu);
         return true;
     }
     
+//**************************************************************************************************
+//called when the user clicks the Send button
+//Add the item to the list when the button is clicked  
+//Modified from following source:
+//Source: http://stackoverflow.com/questions/4540754/add-dynamically-elements-to-a-listview-android
+//Author: Shardul
+//Author's profile page: http://stackoverflow.com/users/514553/shardul
 
-//    called when the user clicks the Send button
-//Add the item to the list when the button is clicked    
     public void addToList(View view){
-    	//Intent intent = new Intent(this, DisplayMessageActivity.class);
-    	//EditText editText = (EditText) findViewById(R.id.edit_message);
-    	//String message = editText.getText().toString();
-    	//intent.putExtra(EXTRA_MESSAGE, message);
-    	//startActivity(intent);
 
     	//to use the textbox
     	EditText enteredText = (EditText) findViewById(R.id.edit_message);
@@ -82,11 +116,15 @@ public class SnapshotShopper extends Activity {
     	//clear the text box so the user can enter another item
     	//findViewById(R.id.edit_message)
     }
+//**************************************************************************************************
     
+//**************************************************************************************************
     //clear the list when user clicks the Clear List button
     //Alerts the user with a confirmation window before clearing the list.
+    //Modified from the following source:
     //Source: http://stackoverflow.com/questions/5127407/android-preference-confirm-dialog
-    //malte at etlam dot com
+    //Author: Maaalte
+    //Author's profile page: http://stackoverflow.com/users/453895/maaalte
     public void clearList(View view){
     	new AlertDialog.Builder(this)
     		.setMessage("Do you really want to clear this list?")
@@ -100,10 +138,8 @@ public class SnapshotShopper extends Activity {
     				listAdapter.notifyDataSetChanged();
     				}
     			})
-    			.setNegativeButton(android.R.string.no, null).show();
-    	
-    	
-    	
+    			.setNegativeButton(android.R.string.no, null).show(); 	
     }
+//**************************************************************************************************
 }
 
