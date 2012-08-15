7e7cdf32335d4d094b8eb9d564fa8cda0d2cf909
Aya Yakura <aya2@pdx.edu>
2012-08-15 15:16:26 -0700

    Minor clean ups

--- SnapshotShopper/gen/com/snapshotshopper/R.java
+++ SnapshotShopper/gen/com/snapshotshopper/R.java
@@ -20,15 +20,16 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int ClearBtn=0x7f080007;
-        public static final int ShoppingList=0x7f080006;
+        public static final int AddBtn=0x7f080008;
+        public static final int ClearBtn=0x7f080005;
+        public static final int ShoppingList=0x7f080004;
         public static final int apply_changes=0x7f080003;
         public static final int delete_item=0x7f080002;
         public static final int edit_item_name=0x7f080000;
-        public static final int edit_message=0x7f080004;
+        public static final int edit_message=0x7f080007;
         public static final int edit_picture=0x7f080001;
-        public static final int imageButton=0x7f080005;
-        public static final int menu_settings=0x7f080008;
+        public static final int imageButton=0x7f080006;
+        public static final int menu_settings=0x7f080009;
     }
     public static final class layout {
         public static final int activity_edit_item_entry=0x7f030000;
